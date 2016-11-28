adb766a10a9422e6428eac5fa7cfd7f1555acc88
TiciaChance <laticia.chance@flatironschool.com>
2016-11-28 08:18:24 -0500

    parsed data

--- WeatherApp/WeatherAPI.swift
+++ WeatherApp/WeatherAPI.swift
@@ -13,13 +13,36 @@ import SwiftyJSON
 class WeatherAPI: NSObject {
     
     let APIkey = "a758550619a710d385f31ca796ab2af1"
-    var cityID = String()
+    var city = "newyork"
     
     func APICall() {
-        Alamofire.request("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=a758550619a710d385f31ca796ab2af1").responseJSON { (response) in
-        
-            print(response.result.value)
-        }
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?q=\(city)&APPID=\(APIkey)").responseJSON { (response) in
+            
+            guard let weather = JSON(data: response.data!).dictionary else {return}
+                
+            print("Date and time: \(weather["dt"]!)")
+            print("City: \(weather["name"]!)")
+            
+            print("Longitude: \(weather["coord"]?["lon"])")
+            print("Latitude: \(weather["coord"]?["lat"])")
+            
+            print("Weather ID: \(weather["weather"]![0]["id"])")
+            print("Weather main: \(weather["weather"]![0]["main"])")
+            print("Weather description: \(weather["weather"]![0]["description"])")
+            print("Weather icon ID: \(weather["weather"]![0]["icon"])")
+            
+            print("Temperature: \(weather["main"]!["temp"])")
+            print("Humidity: \(weather["main"]!["humidity"])")
+            print("Pressure: \(weather["main"]!["pressure"])")
+            
+            print("Cloud cover: \(weather["clouds"]!["all"])")
+            
+            print("Wind direction: \(weather["wind"]!["deg"]) degrees")
+            print("Wind speed: \(weather["wind"]!["speed"])")
+            
+            print("Country: \(weather["sys"]!["country"])")
+            print("Sunrise: \(weather["sys"]!["sunrise"])")
+            print("Sunset: \(weather["sys"]!["sunset"])")        }
     }
 
 }
